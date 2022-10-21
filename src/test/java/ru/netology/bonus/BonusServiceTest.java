778c1be44e401787fa2565159c29a03be6c31ab9
Dmitriy Shilyaev <goodrus1993@yandex.ru>
2022-10-21 21:41:19 +0500

    Initial Commit

---
+++ src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -0,0 +1,20 @@
+package ru.netology.bonus;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class BonusServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv")
+    void shouldCountSquareNumberInGivenRange(long amount, boolean registered, int expected) {
+        BonusService service = new BonusService();
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+}
